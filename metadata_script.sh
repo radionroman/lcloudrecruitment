e29d079fa6a5a4e550d4d12d66e89502831f11cd
Ubuntu <ubuntu@ip-172-31-17-61.eu-west-1.compute.internal>
2024-09-27 13:42:01 +0000

    sends metadata to s3 bucket

--- metadata_script.sh
+++ metadata_script.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 METADATA_URL="http://169.254.169.254/latest/meta-data"
-OUTPUT_FILE="/tmp/tmp_metadata.txt"
+OUTPUT_FILE="/tmp/instance_metadata.txt"
+S3_BUCKET="s3://applicant-task/r2d2/"
 
 INSTANCE_ID=$(curl -s $METADATA_URL/instance-id)
 PUBLIC_IP=$(curl -s $METADATA_URL/public-ipv4)
 PRIVATE_IP=$(curl -s $METADATA_URL/local-ipv4)
 SECURITY_GROUPS=$(curl -s $METADATA_URL/security-groups)
 
+OS_NAME=$(grep "^NAME=" /etc/os-release | cut -d'=' -f2 | tr -d '"')
+OS_VERSION=$(grep "^VERSION=" /etc/os-release | cut -d'=' -f2 | tr -d '"')
+
+USERS=$(grep -E '(/bin/bash|/bin/sh)' /etc/passwd | cut -d':' -f1)
+
 cat <<EOF > $OUTPUT_FILE
 Instance ID: $INSTANCE_ID
 Public IP: $PUBLIC_IP
 Private IP: $PRIVATE_IP
 Security Groups: $SECURITY_GROUPS
+OS Name: $OS_NAME
+OS Version: $OS_VERSION
+Users: $(echo $USERS | tr ' ' ', ')
 EOF
 
-echo "Instance information collected and saved to a file."
+aws s3 cp $OUTPUT_FILE $S3_BUCKET
+
+echo "Instance information collected and saved to s3 bucket."
