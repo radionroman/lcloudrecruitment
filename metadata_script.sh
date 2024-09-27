80af7621e09eb85654e74361945f0fc24eb9b57b
Ubuntu <ubuntu@ip-172-31-17-61.eu-west-1.compute.internal>
2024-09-27 13:21:36 +0000

    script saves to the file

--- metadata_script.sh
+++ metadata_script.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 METADATA_URL="http://169.254.169.254/latest/meta-data"
+OUTPUT_FILE="/tmp/tmp_metadata.txt"
 
-printf "instance-id:"
-curl -s $METADATA_URL/instance-id 
-printf "\npublic-ip:"
-curl -s $METADATA_URL/public-ipv4
-printf "\nprivate_ip:"
-curl -s $METADATA_URL/local-ipv4
-printf "\n"
-curl -s $METADATA_URL/security-groups
-printf "\n"
+INSTANCE_ID=$(curl -s $METADATA_URL/instance-id)
+PUBLIC_IP=$(curl -s $METADATA_URL/public-ipv4)
+PRIVATE_IP=$(curl -s $METADATA_URL/local-ipv4)
+SECURITY_GROUPS=$(curl -s $METADATA_URL/security-groups)
+
+cat <<EOF > $OUTPUT_FILE
+Instance ID: $INSTANCE_ID
+Public IP: $PUBLIC_IP
+Private IP: $PRIVATE_IP
+Security Groups: $SECURITY_GROUPS
+EOF
+
+echo "Instance information collected and saved to a file."
